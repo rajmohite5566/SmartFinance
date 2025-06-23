3ffff1bbbf035aab5a89fb9593fad7347a6e5e15
Atharva Kumkar <kumkaratharva8@gmail.com>
2025-06-23 19:49:42 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,55 +2,102 @@ from tkinter import *
 import customtkinter as ctk
 import tkinter.font as ctkfont 
 
-ctk.set_appearance_mode('light')
-ctk.set_default_color_theme('blue')
+ctk.set_appearance_mode("dark")
 
 top = ctk.CTk()
 top.title("Login Page")
-top.geometry("600x400")
+top.geometry("1080x720")
+top.resizable("false", "false")
 
-underline_font = ctk.CTkFont(family = "Helvetica", size=12,underline=True)  #to underline the forgot password 
+my_font_heading = ctk.CTkFont(family="Gotham-Bold", size = 42)
+my_font_subheading = ctk.CTkFont(family="Gotham-Bold", size = 30)
+my_font_label1 = ctk.CTkFont(family="Gotham-Bold", size = 30)
+my_font_label2 = ctk.CTkFont(family="Gotham-Bold", size = 20)
+my_font_button = ctk.CTkFont(family="Gotham-Bold", size = 20, underline = True)
 
-left_side = ctk.CTkFrame(top,width=500,fg_color='#007bff')
-left_side.pack(side = "left",fill="both",expand=True)
 
-logo = ctk.CTkLabel(left_side, text = "Smart Finance",
-                    font=ctk.CTkFont(size = 25,weight="bold"),
-                    text_color = "white", justify="left")
-logo.place(x=600,y=120)
+top_main_frame = ctk.CTkFrame(master=top, width=1080, height=720, fg_color="#D3ECCD")
+top_main_frame.place(x=0, y=0)
 
-descrp = ctk.CTkLabel(left_side, text="You are the one to use this..",
-        font=ctk.CTkFont(size=25,weight='bold'),
-        text_color='white')
-descrp.place(x=500,y=180)
+logo = ctk.CTkLabel(top_main_frame, 
+                    text = "Smart Finance",
+                    font= my_font_heading,
+                    text_color = "#06923E",
+                    justify="left")
+logo.place(x=50,y=280)
 
-sign_in_box = ctk.CTkFrame(top, width=400, height=450, corner_radius=20, fg_color = "white", border_width=1, border_color="#cccccc",)
-sign_in_box.place(relx = 0.72, rely = 0.5, anchor = "center")
+descrp = ctk.CTkLabel(top_main_frame, 
+                      text="You are the one to use this..",
+                      font = my_font_subheading,
+                      text_color = "#06923E")
+descrp.place(x=50,y=340)
 
-label_1 = ctk.CTkLabel(top, text = "Sign in",font=("Helvetica",20),bg_color='white')
-label_1.place(x=950,y=200)
+sign_in_box = ctk.CTkFrame(top, 
+                           width=400, 
+                           height=400, 
+                           corner_radius=20, 
+                           fg_color = "white",
+                           bg_color = "white", 
+                           border_color="#cccccc",)
+sign_in_box.place(x = 600, y = 150)
 
-label_2 = ctk.CTkLabel(top, text = "Don't have an account?Sign up",bg_color = "white")
-label_2.place(x=950,y=228)
+label_1 = ctk.CTkLabel(master = sign_in_box, 
+                       text = "Sign in",
+                       font=my_font_label1,
+                       text_color = "#06923E")
+label_1.place(x = 20,y = 20)
 
-entry_email = ctk.CTkEntry(top, placeholder_text = "Email",width=250)
-entry_email.place(x=950,y=260)
+label_2 = ctk.CTkLabel(master = sign_in_box, 
+                       text = "Don't have an account? ",
+                       font = my_font_label2,
+                       text_color = "#06923E")
+label_2.place(x = 20, y = 65)
 
-entry_password = ctk.CTkEntry(top, placeholder_text = "Password",show="*",width=250)
-entry_password.place(x=950,y=310)
+signup_button = ctk.CTkButton(master = sign_in_box,
+                             text = "Sign Up",
+                             font = my_font_button,
+                             height = 30,
+                             width = 50,
+                             text_color = "#06923E",
+                             fg_color = "white",
+                             hover_color = "white",
+                             command = "")
+signup_button.place(x = 262,y = 64)
 
-keep_me_check = ctk.BooleanVar()
-my_check= ctk.CTkCheckBox(top, text = "Keep me Logged in",height = 0,width = 0,bg_color='white',
-        variable=keep_me_check, onvalue='on', offvalue='off')
-my_check.place(x=950,y=350)
+line = ctk.CTkFrame(master = sign_in_box, 
+                           width=350, 
+                           height=2, 
+                           bg_color = "#06923E")
+line.place(x = 20, y = 120)
 
-submit_button = ctk.CTkButton(top,text = "Log in Now",height=35,width=250)
-submit_button.place(x=950,y=390)
+entry_email = ctk.CTkEntry(master = sign_in_box,
+                           font = my_font_label2,
+                           placeholder_text = "Email",
+                           width = 350,
+                           height = 40,
+                           fg_color = "white",
+                           border_color = "#06923E")
+entry_email.place(x = 20, y = 150)
 
-forgot_password = ctk.CTkLabel(top, text = "Forgot Password?",cursor="hand2", font=underline_font, text_color="blue",bg_color = "white")
-forgot_password.place(x=1100,y=430)
+password_email = ctk.CTkEntry(master = sign_in_box,
+                           font = my_font_label2,
+                           placeholder_text = "Password",
+                           width = 350,
+                           height = 40,
+                           fg_color = "white",
+                           border_color = "#06923E")
+password_email.place(x = 20, y = 205)
 
-social_media = ctk.CTkFrame(sign_in_box, fg_color = 'white')
-social_media.place(x=950,y=450)
+login_button = ctk.CTkButton(master = sign_in_box,
+                             text = "Login Now",
+                             font = my_font_button,
+                             height = 40,
+                             width = 350,
+                             text_color = "#D3ECCD",
+                             fg_color = "#06923E",
+                             hover_color = "#045B27",
+                             command = "")
+login_button.place(x = 20,y = 275)
+
 
 top.mainloop()
