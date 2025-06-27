ed0db15a7ab9423a3cb2ee8a877a7ee32efc68a3
Atharva Kumkar <kumkaratharva8@gmail.com>
2025-06-27 10:16:12 +0530

    Add files via upload

--- signup.py
+++ signup.py
@@ -15,54 +15,127 @@ signup.resizable(False, False)
 
 my_font_heading = ctk.CTkFont(family="Gotham-Bold", size=42)
 my_font_subheading = ctk.CTkFont(family="Gotham-Bold", size=30)
-my_font_label1 = ctk.CTkFont(family="Gotham-Bold", size=28)
-my_font_label2 = ctk.CTkFont(family="Gotham-Bold", size=18)
+my_font_label1 = ctk.CTkFont(family="Gotham-Bold", size=30)
+my_font_label2 = ctk.CTkFont(family="Gotham-Bold", size=20)
 my_font_button = ctk.CTkFont(family="Gotham-Bold", size=20, underline=True)
 
 
-main_frame = ctk.CTkFrame(master=signup, width=1080, height=720, fg_color="#D3ECCD")
+main_frame = ctk.CTkFrame(master=signup, 
+                          width=1080,
+                          height=720,
+                          fg_color="#D3ECCD")
 main_frame.place(x=0, y=0)
 
 
-logo = ctk.CTkLabel(main_frame, text="SmartFinance 💸", font=my_font_heading, text_color="#06923E")
-logo.place(x=50, y=240)
+logo = ctk.CTkLabel(master=main_frame, 
+                    text="BudgetBee", 
+                    font=my_font_heading, 
+                    text_color="#06923E")
+logo.place(x=40, y=280)
 
-descrp = ctk.CTkLabel(main_frame, text="Let’s get you started.", font=my_font_subheading, text_color="#06923E")
-descrp.place(x=50, y=310)
-
-
-shadow = ctk.CTkFrame(main_frame, width=410, height=460, corner_radius=25, fg_color="#b8dbb8")
-shadow.place(x=604, y=144)
-
-
-signup_box = ctk.CTkFrame(main_frame, width=400, height=450, corner_radius=25,
-                          fg_color="#ffffff", bg_color="#ffffff",
-                          border_color="#06923E", border_width=1)
-signup_box.place(x=600, y=140)
+descrp = ctk.CTkLabel(master=main_frame,
+                      text="Smart finance for smarter students",
+                      font=my_font_subheading,
+                      text_color="#06923E")
+descrp.place(x=40, y=340)
 
 
-strip = ctk.CTkFrame(signup_box, width=400, height=5, fg_color="#06923E")
-strip.place(x=0, y=0)
+shadow = ctk.CTkFrame(master=main_frame,
+                      width=400,
+                      height=450,
+                      fg_color="#b8dbb8")
+shadow.place(x=610, y=165)
 
-title = ctk.CTkLabel(signup_box, text="Sign Up", font=my_font_label1, text_color="#06923E")
+
+signup_box = ctk.CTkFrame(master=main_frame, 
+                          width=400, 
+                          height=450, 
+                          fg_color="#ffffff", 
+                          bg_color="#ffffff")
+signup_box.place(x=600, y=150)
+
+
+"""strip = ctk.CTkFrame(master=signup_box,
+                     width=400,
+                     height=5,
+                     fg_color="#06923E")
+strip.place(x=0, y=0)"""
+
+title = ctk.CTkLabel(master=signup_box,
+                     text="Sign Up",
+                     font=my_font_label1,
+                     text_color="#06923E")
 title.place(x=20, y=20)
 
+sub_title = ctk.CTkLabel(master = signup_box, 
+                       text = "Already Signed up? ",
+                       font = my_font_label2,
+                       text_color = "#06923E")
+sub_title.place(x = 20, y = 65)
 
-entry_name = ctk.CTkEntry(signup_box, font=my_font_label2, placeholder_text="👤 Full Name", width=350, height=40,
-                          fg_color="white", border_color="#06923E", corner_radius=10)
-entry_name.place(x=20, y=80)
+login_button = ctk.CTkButton(master = signup_box,
+                             text = "Login",
+                             font = my_font_button,
+                             height = 30,
+                             width = 50,
+                             text_color = "#06923E",
+                             fg_color = "white",
+                             hover_color = "white",
+                             command = "")
+login_button.place(x = 230,y = 64)
 
-entry_email = ctk.CTkEntry(signup_box, font=my_font_label2, placeholder_text="📧 Email", width=350, height=40,
-                           fg_color="white", border_color="#06923E", corner_radius=10)
-entry_email.place(x=20, y=140)
+line_signup = ctk.CTkFrame(master = signup_box, 
+                           width=350, 
+                           height=2, 
+                           bg_color = "#06923E")
+line_signup.place(x = 20, y = 120)
 
-entry_password = ctk.CTkEntry(signup_box, font=my_font_label2, placeholder_text="🔒 Password", width=350, height=40,
-                              fg_color="white", border_color="#06923E", corner_radius=10, show="*")
-entry_password.place(x=20, y=200)
 
-entry_confirm = ctk.CTkEntry(signup_box, font=my_font_label2, placeholder_text="🔒 Confirm Password", width=350, height=40,
-                             fg_color="white", border_color="#06923E", corner_radius=10, show="*")
-entry_confirm.place(x=20, y=260)
+entry_name = ctk.CTkEntry(master=signup_box,
+                          font=my_font_label2,
+                          placeholder_text="👤 Full Name",
+                          width=350, 
+                          height=40,
+                          fg_color="white",
+                          border_color="#06923E",
+                          text_color = "#06923E",
+                          corner_radius=10)
+entry_name.place(x=20, y=150)
+
+entry_email = ctk.CTkEntry(master=signup_box,
+                           font=my_font_label2,
+                           placeholder_text="📧 Email",
+                           width=350,
+                           height=40,
+                           fg_color="white",
+                           border_color="#06923E",
+                           text_color = "#06923E",
+                           corner_radius=10)
+entry_email.place(x=20, y=205)
+
+entry_password = ctk.CTkEntry(master=signup_box,
+                              font=my_font_label2,
+                              placeholder_text="🔒 Password",
+                              width=350,
+                              height=40,
+                              fg_color="white",
+                              border_color="#06923E",
+                              corner_radius=10,
+                              text_color = "#06923E",
+                              show=".")
+entry_password.place(x=20, y=260)
+
+entry_confirm = ctk.CTkEntry(master=signup_box,
+                             font=my_font_label2,
+                             placeholder_text="🔒 Confirm Password",
+                             width=350,
+                             height=40,
+                             fg_color="white",
+                             border_color="#06923E",
+                             corner_radius=10,
+                             text_color = "#06923E",
+                             show=".")
+entry_confirm.place(x=20, y=315)
 
 
 def validate_and_go():
@@ -85,8 +158,15 @@ def validate_and_go():
         subprocess.Popen([sys.executable, "loginnew.py"])
 
 # Submit Button
-signup_btn = ctk.CTkButton(signup_box, text="Create Account", font=my_font_label2, height=40, width=350,
-                           text_color="#D3ECCD", fg_color="#06923E", hover_color="#045B27", command=validate_and_go)
-signup_btn.place(x=20, y=330)
+signup_btn = ctk.CTkButton(master=signup_box,
+                           text="Create Account",
+                           font=my_font_label2,
+                           height=40,
+                           width=350,
+                           text_color="#D3ECCD",
+                           fg_color="#06923E",
+                           hover_color="#045B27", 
+                           command=validate_and_go)
+signup_btn.place(x=20, y=370)
 
 signup.mainloop()
